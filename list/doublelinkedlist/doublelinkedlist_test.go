fd3ab0493659d0cbb30cd9b3ede362f97559dc80
zongzhenh <huangzongzhuan@gmail.com>
2019-07-03 18:39:23 +0800

    update

--- list/doublelinkedlist/doublelinkedlist_test.go
+++ list/doublelinkedlist/doublelinkedlist_test.go
@@ -1,1 +1,11 @@
 package doublelinkedlist
+
+import "testing"
+
+func TestNew(t *testing.T) {
+	// case1: new empty list
+	list := New()
+	if list.first != nil || list.last != nil || list.size != 0 {
+		t.Error("case1 error: new empty list")
+	}
+}
