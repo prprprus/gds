d5fcb1de598083bab5bcc5a3a1e26d41f234b1c7
zongzhenh <huangzongzhuan@gmail.com>
2019-07-08 15:47:21 +0800

    update

--- queue/linkedlistqueue/iterator.go
+++ queue/linkedlistqueue/iterator.go
@@ -1,1 +1,49 @@
+// Copyright (c) 2019, prprprus All rights reserved.
+// Use of this source code is governed by a BSD-style.
+// license that can be found in the LICENSE file.
+
 package linkedlistqueue
+
+// Iterator represents an iterable structure of queue.
+type Iterator struct {
+	queue *Queue
+	index int
+}
+
+// Iterator returns the iterator object of queue.
+func (queue *Queue) Iterator() *Iterator {
+	return &Iterator{
+		queue: queue,
+		index: -1,
+	}
+}
+
+// Next returns true if the next element exists, false otherwise.
+func (iterator *Iterator) Next() bool {
+	if !iterator.queue.indexInRange(iterator.index + 1) {
+		return false
+	}
+
+	iterator.index++
+	return true
+}
+
+// Begin reset the iterator to the initial status.
+func (iterator *Iterator) Begin() {
+	iterator.index = -1
+}
+
+// Value returns the current value of the element of the iterator.
+func (iterator *Iterator) Value() interface{} {
+	if !iterator.queue.indexInRange(iterator.index) {
+		return nil
+	}
+
+	value, _ := iterator.queue.list.Get(iterator.index)
+	return value
+}
+
+// Index returns the current index of the iterator.
+func (iterator *Iterator) Index() int {
+	return iterator.index
+}
--- queue/linkedlistqueue/linkedlistqueue.go
+++ queue/linkedlistqueue/linkedlistqueue.go
@@ -1,1 +1,79 @@
+// Copyright (c) 2019, prprprus All rights reserved.
+// Use of this source code is governed by a BSD-style.
+// license that can be found in the LICENSE file.
+
+// Package linkedlistqueue implements the linked list queue.
+// Structure is not concurrent safe.
+// Reference: https://en.wikipedia.org/wiki/Queue_(abstract_data_type)
 package linkedlistqueue
+
+import (
+	"errors"
+
+	"github.com/prprprus/ds/list/singlylinkedlist"
+)
+
+var (
+	// ErrPop is returned when the queue is empty
+	ErrPop = errors.New("queue is empty")
+)
+
+// Queue represents a linked list  queue structure.
+type Queue struct {
+	list *singlylinkedlist.List
+}
+
+// New linked list  queue.
+func New() *Queue {
+	return &Queue{
+		list: &singlylinkedlist.List{},
+	}
+}
+
+// Queue Interface
+
+// Put value into the queue.
+func (queue *Queue) Put(value interface{}) {
+	queue.list.Append(value)
+}
+
+// Get the value from the top of the queue and delete the value.
+func (queue *Queue) Get() (interface{}, error) {
+	if queue.Size() == 0 {
+		return nil, ErrPop
+	}
+
+	value, _ := queue.list.Get(0)
+	queue.list.Remove(0)
+	return value, nil
+}
+
+// Check if the index is within the length of the list.
+func (queue *Queue) indexInRange(index int) bool {
+	if index >= 0 && index < queue.list.Size() {
+		return true
+	}
+	return false
+}
+
+// Container Interface
+
+// Empty returns true if the queue is empty, otherwise returns false.
+func (queue *Queue) Empty() bool {
+	return queue.list.Size() == 0
+}
+
+// Size returns the size of the queue.
+func (queue *Queue) Size() int {
+	return queue.list.Size()
+}
+
+// Clear the queue.
+func (queue *Queue) Clear() {
+	queue.list.Clear()
+}
+
+// Values returns the values of queue.
+func (queue *Queue) Values() []interface{} {
+	return queue.list.Values()
+}
