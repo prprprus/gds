cd8268376dff096861d718da1673058ddb33828e
zongzhenh <huangzongzhuan@gmail.com>
2019-07-08 16:00:54 +0800

    update

--- queue/queue.go
+++ queue/queue.go
@@ -1,1 +1,12 @@
+// Copyright (c) 2019, prprprus All rights reserved.
+// Use of this source code is governed by a BSD-style.
+// license that can be found in the LICENSE file.
+
 package queue
+
+// Queue Interface
+type Queue interface {
+	Put(value interface{})
+	Get() (interface{}, error)
+	indexInRange(index int) bool
+}
---
+++ tmptest.sh
@@ -0,0 +1,9 @@
+source ~/.bash_profile
+
+go test -v github.com/prprprus/ds/list/singlylinkedlist
+go test -v github.com/prprprus/ds/list/doublelinkedlist
+go test -v github.com/prprprus/ds/list/arraylist
+go test -v github.com/prprprus/ds/stack/arraystack
+go test -v github.com/prprprus/ds/stack/linkedliststack
+go test -v github.com/prprprus/ds/queue/arrayqueue
+go test -v github.com/prprprus/ds/queue/linkedlistqueue
